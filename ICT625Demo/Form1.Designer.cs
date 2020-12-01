3aeddef48932087dc92ad8ad690a71dab0669dee
nidialal <nidialal@gmail.com>
2020-12-02 10:33:06 +1300

    commit 2 by Nidia

--- ICT625Demo/Form1.Designer.cs
+++ ICT625Demo/Form1.Designer.cs
@@ -40,7 +40,6 @@ namespace ICT625Demo
             this.label1.Size = new System.Drawing.Size(38, 12);
             this.label1.TabIndex = 0;
             this.label1.Text = "label1";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
             // 
